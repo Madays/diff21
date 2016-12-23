d9d9adc2b640efdc60c5838d5682a0fe1171fc08
Madays <maday.choque@gmail.com>
2016-12-23 11:57:28 -0500

    corrigindo la formula de diferencia absoluta

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ function diff21() {
     var in_n = parseFloat(document.getElementById("in_n").value);
     var diff;
     if (in_n>21){
-        diff=2*(in_n-21);        document.getElementById("resultado").innerHTML="Es el doble de la diferencia absoluta: " + diff;                
+        diff=2*(in_n+21);        document.getElementById("resultado").innerHTML="Es el doble de la diferencia absoluta: " + diff;                
     } else {
-        diff=-1*(in_n-21);        document.getElementById("resultado").innerHTML="La diferencia absoluta es: " + diff;   
+        diff=-1*(in_n+21);        document.getElementById("resultado").innerHTML="La diferencia absoluta es: " + diff;   
     }        
     }   
 
